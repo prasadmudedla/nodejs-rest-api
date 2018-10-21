defaa0885ccfb13e117ee08c92b91183c63914c4
biswajit <biswajit.pattanayak@gmail.com>
2018-10-21 17:54:27 +0530

    Biswajit: add timeout of 5s

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var timeout = require('connect-timeout');
+
+app.use(timeout('5s'));
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
